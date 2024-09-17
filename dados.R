4ee910e3d6c1c24d84b96135cb775f814e341129
Beatriz Milz <milz.bea@gmail.com>
2024-09-17 08:39:16 -0300

    preparando os dados

--- dados.R
+++ dados.R
@@ -2,22 +2,23 @@ library(sidrar)
 
 # Sugestão de dados feita pelo Rafael: ------
 # https://sidra.ibge.gov.br/Tabela/4092
-# Dados trimestrais da PNADc: 
+# Dados trimestrais da PNADc:
 # "Pessoas de 14 anos ou mais de idade,
 # por condição em relação à força de trabalho e condição de ocupação"
 # Codigo: 4092
 
-# Com o codigo abaixo, a baixa essa tabela que traz pop ocupada e desocupada 
+# Com o codigo abaixo, a baixa essa tabela que traz pop ocupada e desocupada
 # para cada UF em diferentes trimentes.
-# Acho que um dos objetivos do exercicio poderia ser por exemplo calcular a 
-# taxa de desocupação de cada estado e cada região ao longo do tempo. 
+# Acho que um dos objetivos do exercicio poderia ser por exemplo calcular a
+# taxa de desocupação de cada estado e cada região ao longo do tempo.
 # Voces vao ver que a tabela é bem 'untidy', entao dá bastante pano pra manga.
 
+# Buscando informação desta tabela no SIDRA
 informacoes_tabela_4092 <- sidrar::info_sidra(4092)
 
-
-periodos_disponiveis <- informacoes_tabela_4092$period |> 
-  stringr::str_split(pattern = ", ") |> 
+# Quais são os períodos disponíveis?
+periodos_disponiveis <- informacoes_tabela_4092$period |>
+  stringr::str_split(pattern = ", ") |>
   unlist()
 
 
@@ -38,19 +39,26 @@ dados_4092 <- sidrar::get_sidra(
 
 
 prep_dados_4092 <- dados_4092 |>
+  # Transformando em tibble para facilitar a manipulação dos dados
   tibble::as_tibble() |>
+  # Limpando os nomes das colunas (coloca os nomes das colunas em minúsculas e substitui espaços por "_")
   janitor::clean_names() |>
+  # Filtrando apenas a variável de interesse (pessoas de 14 anos ou mais de idade)
   dplyr::filter(variavel_codigo == 1641) |>
-  
   dplyr::mutate(
-    condicao = dplyr::case_when(
-      condicao_em_relacao_a_forca_de_trabalho_e_condicao_de_ocupacao == "Total" ~ "total_mil_pessoas",
-      condicao_em_relacao_a_forca_de_trabalho_e_condicao_de_ocupacao == "Força de trabalho" ~ "forca_de_trabalho_total_mil_pessoas",
-      condicao_em_relacao_a_forca_de_trabalho_e_condicao_de_ocupacao == "Força de trabalho - ocupada" ~ "forca_de_trabalho_ocupada_mil_pessoas",
-      condicao_em_relacao_a_forca_de_trabalho_e_condicao_de_ocupacao == "Força de trabalho - desocupada" ~ "forca_de_trabalho_desocupada_mil_pessoas",
-      condicao_em_relacao_a_forca_de_trabalho_e_condicao_de_ocupacao == "Fora da força de trabalho" ~ "fora_da_forca_de_trabalho_mil_pessoas",
-    )
+    # Criando uma nova coluna chamada "condição", baseada nos valores presentes
+    # em condicao_em_relacao_a_forca_de_trabalho_e_condicao_de_ocupacao
+    # Queremos deixar os nomes das categorias de forma que facilite a virar nomes
+    # de colunas posteriormentte
+    condicao = janitor::make_clean_names(
+      condicao_em_relacao_a_forca_de_trabalho_e_condicao_de_ocupacao,
+      allow_dupes = TRUE
+    ),
+    # colando o texto "_mil_pessoas" no final de cada valor de condicao
+    # assim fica mais claro que essas colunas representam a quantidade de pessoas (em milhares)
+    condicao = paste0(condicao, "_mil_pessoas"),
   ) |>
+    # Removendo colunas
   dplyr::select(
     -c(
       condicao_em_relacao_a_forca_de_trabalho_e_condicao_de_ocupacao_codigo,
@@ -61,17 +69,23 @@ prep_dados_4092 <- dados_4092 |>
       nivel_territorial,
       unidade_de_medida_codigo,
       unidade_de_medida
-    )
-  ) |>
+    )) |> 
+  # Transformando os dados de formato longo para largo
+  # Agora cada linha representa um trimestre de um estado
+  # Os valores usados para as colunas virão da coluna "condicao"
+  # e os valores usados para as células virão da coluna "valor"
   tidyr::pivot_wider(names_from = condicao, values_from = valor) |>
+  # Reordenando as colunas, para ficar mais fácil de endender
   dplyr::relocate(
     forca_de_trabalho_ocupada_mil_pessoas,
     forca_de_trabalho_desocupada_mil_pessoas,
-    forca_de_trabalho_total_mil_pessoas,
+    forca_de_trabalho_mil_pessoas,
     fora_da_forca_de_trabalho_mil_pessoas,
     total_mil_pessoas,
     .after = tidyselect::everything()
   ) |>
+  # Criando novas colunas para o ano, trimestre, mês de início e fim do trimestre
+  # Isso poderia ser uma função!
   dplyr::mutate(
     ano = stringr::str_sub(trimestre_codigo, 1, 4),
     trimestre_numero = stringr::str_sub(trimestre_codigo, 6, 7),
@@ -95,10 +109,14 @@ prep_dados_4092 <- dados_4092 |>
     ),
     .before = tidyselect::everything()
   ) |>
+  # Removendo colunas
   dplyr::select(-c(trimestre_mes_inicio, trimestre_mes_fim)) |>
+  # Reordenando colunas
   dplyr::relocate(tidyselect::starts_with("trimestre"), .after = ano) |>
+  # Renomeando a coluna trimestre
   dplyr::rename(trimestre_texto = trimestre) |>
-  dplyr::arrange(trimestre_data_inicio, unidade_da_federacao) 
+  # Ordenando as linhas
+  dplyr::arrange(trimestre_data_inicio, unidade_da_federacao)
 
 
 
@@ -108,17 +126,21 @@ prep_dados_4092 <- dados_4092 |>
 # Salvando os dados brutos em vários formatos, para usar no curso!
 readr::write_rds(dados_4092, "dados/sidrar_4092_bruto.rds")
 
-readr::write_csv(
-  dados_4092,
-  "dados/sidrar_4092_bruto.csv"
-)
+readr::write_csv(dados_4092, "dados/sidrar_4092_bruto.csv")
 
-readr::write_csv2(
-  dados_4092,
-  "dados/sidrar_4092_bruto_2.csv"
-)
+readr::write_csv2(dados_4092, "dados/sidrar_4092_bruto_2.csv")
 
-writexl::write_xlsx(
-  dados_4092,
-  "dados/sidrar_4092_bruto.xlsx"
-)
+writexl::write_xlsx(dados_4092, "dados/sidrar_4092_bruto.xlsx")
+
+
+# Salvando os dados preparados em vários formatos, para usar no curso!
+
+readr::write_rds(prep_dados_4092, "dados/sidrar_4092.rds")
+
+readr::write_csv(prep_dados_4092, "dados/sidrar_4092.csv")
+
+readr::write_csv2(prep_dados_4092, "dados/sidrar_4092_2.csv")
+
+writexl::write_xlsx(prep_dados_4092, "dados/sidrar_4092.xlsx")
+
+
