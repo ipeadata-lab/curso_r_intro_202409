efd5d2cdcd2482decdf64afc4685123206b4bcd8
Beatriz Milz <milz.bea@gmail.com>
2024-10-24 21:09:31 -0300

    prep aula 8

---
+++ scripts/aula-8-prep.R
@@ -0,0 +1,18 @@
+# Preparação para a próxima aula!
+# Importante instalar o censobr:
+install.packages("censobr")
+
+# Carregando o pacote
+library(censobr)
+
+# Leitura dos dados de população em 2010, para deixar em cache
+populacao_2010 <- read_population(year = 2010)
+
+# caso tenha sido corrompido, usar cache = FALSE
+# populacao_2010 <- read_population(year = 2010, cache = FALSE)
+
+nrow(populacao_2010)
+# [1] 20635472          
+
+ncol(populacao_2010)
+# [1] 251
